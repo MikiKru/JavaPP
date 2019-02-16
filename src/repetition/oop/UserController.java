ed8e2779f8e33f82960bd541f796788b09e4041e
MikiKru <michal_kruczkowski@o2.pl>
2019-02-16 11:35:54 +0100

    repetition OOP - controller

--- src/repetition/oop/UserController.java
+++ src/repetition/oop/UserController.java
@@ -1,4 +1,26 @@
 package repetition.oop;
 
 public class UserController {
+    // deklaracja tablicy użytkowników
+    User users_table [] = new User[100];
+    int index = 0;
+    // dodawanie użytkownika do tablicy
+    public void addUser(String name,
+                        String lastame,
+                        String email,
+                        String phone,
+                        String login,
+                        String password){
+        User u = new User(
+                "Michał",
+                "Kruczkowski",
+                "mk@mk.pl",
+                "123456654",
+                "mk",
+                "mk");
+        users_table[index] = u;
+        index++;
+        System.out.println("Pomyślnie zarejestrowano: \n"+u);
+    }
+
 }
