1733625ad73267bcc5d9ad7e7d71c94e752590fb
MikiKru <michal_kruczkowski@o2.pl>
2019-02-16 16:01:48 +0100

    alg9

---
+++ src/alg10/Matrix.java
@@ -0,0 +1,19 @@
+package alg10;
+
+public class Matrix {
+
+    int matrix [][] = { {1,1,1}, {2,2,2}, {3,3,3} };
+    int matrix3D [][][] ={
+            {{1,2},{3,4}},
+            {{5,6},{7,9}}
+                        };
+
+    public static void main(String[] args) {
+        Matrix m = new Matrix();
+//        System.out.println(m.matrix3D[1][1][0]);
+//        System.out.println(m.matrix3D[0][0][1]);
+//        System.out.println(m.matrix3D[1][0][1]);
+    }
+
+
+}
